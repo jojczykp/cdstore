622aeec13d3fe5d8a55d27d5198c7324783403f7
jojczykp <pawel.jojczyk@gmail.com>
2016-11-17 01:03:39 +0000

    release config update

--- bin/cdstore.sh
+++ bin/cdstore.sh
@@ -4,19 +4,19 @@ SERVICE_NAME=cdstore
 
 PATH_TO_JAR=/usr/lib/${SERVICE_NAME}/${SERVICE_NAME}.jar
 PATH_TO_CFG=/usr/lib/${SERVICE_NAME}/${SERVICE_NAME}.yml
-PID_PATH_NAME=/var/run/${SERVICE_NAME}.pid
+PATH_TO_PID=/var/run/${SERVICE_NAME}/${SERVICE_NAME}.pid
 
-STDOUT=/var/log/${SERVICE_NAME}/${SERVICE_NAME}.out
-STDERR=/var/log/${SERVICE_NAME}/${SERVICE_NAME}.err
+PATH_TO_OUT=/var/log/${SERVICE_NAME}/${SERVICE_NAME}.out
+PATH_TO_ERR=/var/log/${SERVICE_NAME}/${SERVICE_NAME}.err
 
 java -version || { echo "ERROR: java executable not found" ; exit 1; }
 
 case $1 in
     start)
         echo "Starting ${SERVICE_NAME} ..."
-        if [ ! -f ${PID_PATH_NAME} ]; then
-            nohup java -jar ${PATH_TO_JAR} ${PATH_TO_CFG} 2>> ${STDERR} >> ${STDOUT} &
-            echo $! > ${PID_PATH_NAME}
+        if [ ! -f ${PATH_TO_PID} ]; then
+            nohup java -jar ${PATH_TO_JAR} ${PATH_TO_CFG} 2>> ${PATH_TO_ERR} >> ${PATH_TO_OUT} &
+            echo $! > ${PATH_TO_PID}
             echo "${SERVICE_NAME} started ..."
         else
             echo "${SERVICE_NAME} is already running ..."
@@ -24,27 +24,27 @@ case $1 in
     ;;
 
     stop)
-        if [ -f ${PID_PATH_NAME} ]; then
-            PID=$(cat ${PID_PATH_NAME});
+        if [ -f ${PATH_TO_PID} ]; then
+            PID=$(cat ${PATH_TO_PID});
             echo "${SERVICE_NAME} stopping ..."
             kill ${PID};
             echo "${SERVICE_NAME} stopped ..."
-            rm ${PID_PATH_NAME}
+            rm ${PATH_TO_PID}
         else
             echo "${SERVICE_NAME} is not running ..."
         fi
     ;;
 
     restart)
-        if [ -f ${PID_PATH_NAME} ]; then
-            PID=$(cat ${PID_PATH_NAME});
+        if [ -f ${PATH_TO_PID} ]; then
+            PID=$(cat ${PATH_TO_PID});
             echo "${SERVICE_NAME} stopping ...";
             kill ${PID};
             echo "${SERVICE_NAME} stopped ...";
-            rm ${PID_PATH_NAME}
+            rm ${PATH_TO_PID}
             echo "${SERVICE_NAME} starting ..."
-            nohup java -jar ${PATH_TO_JAR} ${PATH_TO_CFG} 2>> ${STDERR} >> ${STDOUT} &
-                        echo $! > ${PID_PATH_NAME}
+            nohup java -jar ${PATH_TO_JAR} ${PATH_TO_CFG} 2>> ${PATH_TO_ERR} >> ${PATH_TO_OUT} &
+                        echo $! > ${PATH_TO_PID}
             echo "${SERVICE_NAME} started ..."
         else
             echo "${SERVICE_NAME} is not running ..."
