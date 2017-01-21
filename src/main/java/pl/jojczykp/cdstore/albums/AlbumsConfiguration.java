f5118e1051b9aa5cb5377f2aa48a26296d2c39e4
jojczykp <pawel.jojczyk@gmail.com>
2017-01-21 23:00:04 +0000

    Get rid of unnecessary field

--- src/main/java/pl/jojczykp/cdstore/albums/AlbumsConfiguration.java
+++ src/main/java/pl/jojczykp/cdstore/albums/AlbumsConfiguration.java
@@ -1,16 +1,10 @@
 package pl.jojczykp.cdstore.albums;
 
-import com.amazonaws.regions.Regions;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class AlbumsConfiguration {
-
-	@NotEmpty
-	@JsonProperty
-	@Getter
-	private Regions region;
 
 	@NotEmpty
 	@JsonProperty
