d3bcd74bd5ab6b894a743b04c17bf1b593cde507
jojczykp <pawel.jojczyk@gmail.com>
2017-02-12 02:30:31 +0000

    get rid of premature service stopping

--- jenkins/pre_build.sh
+++ jenkins/pre_build.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
 
-DST="/usr/lib/cdstore"
-BIN="${DST}/cdstore.sh"
-
-bash ${BIN} stop  || { echo "Stopping service failed"; exit 1; }
-
 git branch -r | awk -F/ '/\/release\//{print $2"/"$3}' | xargs -I {} git push origin :{} \
     || { echo "Deleting obsolete release branches failed"; exit 1; }
